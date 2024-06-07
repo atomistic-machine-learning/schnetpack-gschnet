7294635cd4d00229f9bc655fd9c053835e238502
Niklas Gebauer <n.wa.gebauer@gmail.com>
2024-06-07 13:51:55 +0200

    add custom resolver to automatically compute refresh rate that updates every 20 percent of each epoch (i.e. 5 times per epoch)

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 import schnetpack.cli as cli
+import math
+from omegaconf import OmegaConf
+
+OmegaConf.register_new_resolver(
+    "compute_refresh_rate",
+    lambda num_train, batch_size, updates: max(
+        1, int(math.ceil(num_train / batch_size) / updates)
+    ),
+)
 
 if __name__ == "__main__":
     cli.train()
